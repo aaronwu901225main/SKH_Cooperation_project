62261ed0a4b56039235ee4f3c2b3705fb543ed14
aaronwu901225main <aaronwu901225main@gmail.com>
2025-02-20 16:38:35 +0800

    Update AVF_MUE_th_DL.py

--- Tree_Based_Uncertainty_Analysis/extend_comp_exp_75_to_1_normal_weight/AVF_MUE_th_DL.py
+++ Tree_Based_Uncertainty_Analysis/extend_comp_exp_75_to_1_normal_weight/AVF_MUE_th_DL.py
@@ -179,8 +179,8 @@ for mul in tqdm(uncertain_mul, desc=f"uncertain_threshold :"):
             npv_avf_list = []
             thres_list = []
     #         print(f'*********\nAVF :{threshold_output_index+1}/3\n{noise_level}\n*********')
-            model = VotingClassifier(estimators=[('mlp', mlp),
-                                                 ('mlp', mlp),
+            model = VotingClassifier(estimators=[('mlp_1', mlp),
+                                                 ('mlp_2', mlp),
                                                  ('xgb2', XGBClassifier(random_state=42,scale_pos_weight=data_scale_pos_weight,eta=0.001,n_estimators=50))#,eta=0.016,n_estimators=100
                                                 ], voting='soft', weights=[1, 1, 1])#
             model.fit(X_train, y_train)
